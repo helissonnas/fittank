fa3e07a6822d18968f30190bad567792d06a6114
Hélisson Luiz <helisson.nascimento@ccc.ufcg.edu.br>
2018-05-27 17:56:21 -0300

    reoisitory de academia criado

---
+++ src/main/java/br/com/fittank/fittank/repository/AcademiaRepository.java
@@ -0,0 +1,9 @@
+package br.com.fittank.fittank.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.fittank.fittank.model.Academia;
+
+public interface AcademiaRepository extends JpaRepository<Academia, String> {
+
+}
