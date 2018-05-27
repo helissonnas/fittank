735b37feab3946cc2031298dbafcd859bff41abd
Hélisson Luiz <helisson.nascimento@ccc.ufcg.edu.br>
2018-05-27 17:54:03 -0300

    primeira rota

---
+++ src/main/java/br/com/fittank/fittank/controller/AcademiaController.java
@@ -0,0 +1,10 @@
+package br.com.fittank.fittank.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/academia")
+public class AcademiaController {
+
+}
